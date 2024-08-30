90f5d1012bb022657db4129bfb5a31a206cf59eb
Miguelin02 <miguelangel.vr0202@gmail.com>
2024-08-29 19:49:19 -0500

    Inicialización del proyecto DESARROLLO_VII

---
+++ TALLERES/TALLERES_3/strlen.php
@@ -0,0 +1,6 @@
+
+cd C:/laragon/www/TALLERES/TALLER_3
+git add strlen.php
+git commit -m "Inicialización del archivo strlen.php para el TALLER_3"
+git push origin main
+          
---
+++ TALLERES/TALLER_2/index.php
@@ -0,0 +1,11 @@
+cd C:/laragon/www
+git init
+git add .
+git commit -m "Inicialización del proyecto DESARROLLO_VII"
+                        
+
+git remote add origin https://github.com/Miguelin02/DESARROLLO_VII_MIGUEL_VALLEJO.git
+git branch -M main
+git push -u origin main -f
+                        
+
