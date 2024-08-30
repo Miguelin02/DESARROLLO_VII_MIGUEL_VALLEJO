dad1c707b8bfe1c787679f4616c309647ce385a5
Miguelin02 <miguelangel.vr0202@gmail.com>
2024-08-29 19:52:16 -0500

    Agregada estructura básica de PHP

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,11 +1,6 @@
-cd C:/laragon/www
-git init
-git add .
-git commit -m "Inicialización del proyecto DESARROLLO_VII"
-                        
 
-git remote add origin https://github.com/Miguelin02/DESARROLLO_VII_MIGUEL_VALLEJO.git
-git branch -M main
-git push -u origin main -f
-                        
+<?php
+// Tu código PHP irá aquí
 
+?>
+                        
